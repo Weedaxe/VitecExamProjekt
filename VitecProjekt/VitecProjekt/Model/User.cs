753fa561989ab4269d5e037560707f3652be7d2b
mich5c18 <mich5c18@edu.ucl.dk>
2019-09-24 10:26:30 +0200

    User Model.
    Made a model folder and added the model User.

---
+++ VitecProjekt/VitecProjekt/Model/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VitecProjekt.Model
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+}
