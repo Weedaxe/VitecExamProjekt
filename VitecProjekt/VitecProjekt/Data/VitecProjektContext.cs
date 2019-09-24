659fe32213d1a2b9cb7b37db62aead5d97195de4
mich5c18 <mich5c18@edu.ucl.dk>
2019-09-24 11:33:47 +0200

    Scaffolded Users.

---
+++ VitecProjekt/VitecProjekt/Data/VitecProjektContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using VitecProjekt.Model;
+
+namespace VitecProjekt.Models
+{
+    public class VitecProjektContext : DbContext
+    {
+        public VitecProjektContext (DbContextOptions<VitecProjektContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<VitecProjekt.Model.User> User { get; set; }
+    }
+}
