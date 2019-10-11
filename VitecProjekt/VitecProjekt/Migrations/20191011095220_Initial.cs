f7730391d351996d2c22374d17b0804ebdca36d4
Jes Lindekilde Johannsen <jesx0347@edu.eal.dk>
2019-10-11 11:56:52 +0200

    initial migrate

---
+++ VitecProjekt/VitecProjekt/Migrations/20191011095220_Initial.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VitecProjekt.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Package",
+                columns: table => new
+                {
+                    PackageID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    product = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Package", x => x.PackageID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Package");
+        }
+    }
+}
